94697fd19ac5608d596f7ff72e2068fa1c279ce7
bramjanssens <bram.janssens@infosupport.com>
2020-05-27 15:05:30 +0200

    StudentsResourceIT extended to test JPA features.

--- src/test/java/org/example/resources/StudentsResourceIT.java
+++ src/test/java/org/example/resources/StudentsResourceIT.java
@@ -1,23 +1,29 @@
 package org.example.resources;
 
 import org.example.App;
-import org.example.domain.Students;
+import org.example.dao.StudentDao;
+import org.example.domain.Student;
 import org.example.services.StudentService;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
+import java.io.File;
 import java.net.URL;
 
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.example.domain.Values.JANSSENS;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(Arquillian.class)
@@ -37,24 +43,44 @@ public class StudentsResourceIT {
     public static Archive<?> createDeployment() {
         WebArchive archive = ShrinkWrap.create(WebArchive.class, "test.war")
                 .addClass(App.class) // dont forget!
+                .addClass(StudentsResource.class)
                 .addClass(StudentService.class)
-                .addPackage(StudentsResource.class.getPackage())
-                .addPackage(Students.class.getPackage());
+                .addClass(StudentDao.class)
+                .addPackage(Student.class.getPackage())
+                .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsLibraries(hibernate());
+
         System.out.println(archive.toString(true));
         return archive;
     }
 
+    private static File[] hibernate() {
+        return Maven.resolver()
+                .loadPomFromFile("pom.xml")
+                .resolve("org.hibernate:hibernate-entitymanager")
+                .withTransitivity()
+                .asFile();
+    }
+
     @Test
     public void getStudentsReturnsStudents() {
-        System.out.println("studentsResource=" + studentsResource);
         String message = ClientBuilder.newClient()
                 .target(studentsResource)
                 .request(MediaType.APPLICATION_JSON)
                 .get(String.class);
 
-        System.out.println(message);
-
         assertThat(message, containsString("{\"students\":["));
+    }
+
+    @Test
+    public void postStudentAddsStudentToResource() {
+        Student response = ClientBuilder.newClient()
+                .target(studentsResource)
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(JANSSENS), Student.class);
+
+        assertThat(response.getId(), is(not(nullValue())));
     }
 
 }
